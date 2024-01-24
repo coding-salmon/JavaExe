373fdf22fb6ce657ec4e80a8cda65a1a2ff0f6b6
NCP08 <bitcamp1@bitcamp>
2024-01-24 16:19:24 +0900

    챕터2

---
+++ Ch02_Variable/src/variable01/DataType.java
@@ -0,0 +1,20 @@
+package variable01;
+
+public class DataType {
+	private void mian() {
+		int num 100;
+		double dnum = 3.14;
+		char ch = 'a';
+		String name = "홍길동";
+		boolean isRun = true;
+		String str = "a";
+		String name1 = "Albert"
+				
+		System.out.println(num);		
+		System.out.println(dnum);		
+		System.out.println(ch);		
+		System.out.println(name);		
+		System.out.println(isRun);		
+			}
+
+}
