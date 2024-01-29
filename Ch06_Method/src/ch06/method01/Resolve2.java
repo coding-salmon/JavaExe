453f899b3585d3cf2f03476fec1d23352c1d3204
NCP08 <bitcamp1@bitcamp>
2024-01-29 20:37:12 +0900

    자습

--- Ch06_Method/src/ch06/method01/Resolve2.java
+++ Ch06_Method/src/ch06/method01/Resolve2.java
@@ -5,17 +5,28 @@ import java.util.Scanner;
 public class Resolve2 {
 	
 	
-	public static int only(int num0 , int num1) {
+	public static  void difNum(int num0 , int num1) {
 		
+		int difNum = num0 - num1;
+		int neverDifNum =0;
+		if(difNum<0) {
+			neverDifNum = -difNum;
+			
+		}else {neverDifNum = difNum;
+			
+		}
+		System.out.println("두수의 차의 절대값은" +neverDifNum);
 	}
 	public static void main(String[] args) {
-		Scanner sc = Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("첫번째 정수를 입력하시오>>");
 		int num0 = sc.nextInt();	
 	
 		System.out.println("두번째 정수를 입력하시오>>");
 		int num1 = sc.nextInt();	
-	
+		
+		difNum(num0,num1);
+		sc.close();	
 
 	}
 
